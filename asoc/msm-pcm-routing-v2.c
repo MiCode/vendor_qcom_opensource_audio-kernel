f5bfb55847fd9e04baa5cbde2698531e059c7e8d
Ye Xuebing <yexuebing@xiaomi.com>
2019-08-01 17:03:49 +0800

    audio: cs35l41 bringup (4. adsp part)
    UMI-393
    
    cs35l41 bringup on adsp part.
    
    Change-Id: Iad2f5306f66a2d6140b9578c7b5b6490f66f3453
    Signed-off-by: lugandong <lugandong@xiaomi.com>

--- asoc/msm-pcm-routing-v2.c
+++ asoc/msm-pcm-routing-v2.c
@@ -29758,6 +29758,10 @@ static const struct snd_pcm_ops msm_routing_pcm_ops = {
 	.close          = msm_pcm_routing_close,
 	.prepare        = msm_pcm_routing_prepare,
 };
+#ifdef CONFIG_MSM_CSPL
+	extern void msm_crus_pb_add_controls(struct snd_soc_component *platform);
+#endif
+
 
 #ifdef CONFIG_DOA_PARAMS_ENABLED
 void msm_routing_add_doa_control(struct snd_soc_component *component)
@@ -29861,6 +29865,10 @@ static int msm_routing_probe(struct snd_soc_component *component)
 			port_multi_channel_map_mixer_controls,
 			ARRAY_SIZE(port_multi_channel_map_mixer_controls));
 	elliptic_add_component_controls(component);
+	
+#ifdef CONFIG_MSM_CSPL
+	msm_crus_pb_add_controls(component);
+#endif
 
 	return 0;
 }
