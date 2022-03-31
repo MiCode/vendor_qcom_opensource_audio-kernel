b2c552a544331d34ee911e08cc7f317f0e42dfe8
sasevs <quic_sasevs@quicinc.com>
2022-03-31 18:10:44 +0530

    audio-kernel: call subdirectory make
    Call subdirectory Andorid.mk
    This help build legacy code
    
    Change-Id: I55930e1154827a4d2e4a5103b8c5fa8644f38d10
    Signed-off-by: sasevs <quic_sasevs@quicinc.com>

--- audio_kernel_vendor_board.mk
+++ audio_kernel_vendor_board.mk
@@ -1,2 +1,5 @@
 include vendor/qcom/opensource/audio-kernel/audio_kernel_modules.mk
+ifeq ($(ENABLE_AUDIO_LEGACY_TECHPACK),true)
+include vendor/qcom/opensource/audio-kernel/legacy/audio_kernel_modules.mk
+endif
 BOARD_VENDOR_KERNEL_MODULES += $(AUDIO_KERNEL_MODULES)
