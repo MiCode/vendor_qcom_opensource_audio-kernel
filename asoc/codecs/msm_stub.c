422607dafca69fa202c70bcc3f66fbf74445fec4
Anirudh Mahto <quic_amahto@quicinc.com>
2022-09-14 15:05:56 +0530

    asco: msm-stub: Extend bit-width support to 24 and 32 bit
    Extend bit-width support to 24 and 32 bit for TDM, MI2S and AUX PCM  interface
    
    Change-Id: I0c79df64881f2d96a16196e7fbef0cc177af1021
    Signed-off-by: Anirudh Mahto <quic_amahto@quicinc.com>

--- asoc/codecs/msm_stub.c
+++ asoc/codecs/msm_stub.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2011-2014, 2017-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
+
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/module.h>
@@ -20,7 +22,10 @@ static struct snd_soc_dai_driver msm_stub_dais[] = {
 			.channels_min = 1,
 			.channels_max = 8,
 			.rates = SNDRV_PCM_RATE_8000_48000,
-			.formats = SNDRV_PCM_FMTBIT_S16_LE,
+			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
+				    SNDRV_PCM_FMTBIT_S24_LE |
+				    SNDRV_PCM_FMTBIT_S24_3LE |
+				    SNDRV_PCM_FMTBIT_S32_LE),
 		},
 	},
 	{
@@ -31,7 +36,9 @@ static struct snd_soc_dai_driver msm_stub_dais[] = {
 			.channels_max = 8,
 			.rates = SNDRV_PCM_RATE_8000_48000,
 			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
-				    SNDRV_PCM_FMTBIT_S24_LE),
+				    SNDRV_PCM_FMTBIT_S24_LE |
+				    SNDRV_PCM_FMTBIT_S24_3LE |
+				    SNDRV_PCM_FMTBIT_S32_LE),
 		},
 	},
 };
