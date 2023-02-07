b1da4ef67d57c78183cc016dd8ecaec2a9baddfd
Eric Rosas <quic_erosas@quicinc.com>
2023-02-07 15:19:31 -0800

    soc: Remove internal.h header
    Remove soc/internal.h, which
    is a duplicate of the internal regmap/internal.h.
    
    Change-Id: I05b30b101382e68ced26343ef1278cecfd1b0ed3
    Signed-off-by: Eric Rosas <quic_erosas@quicinc.com>

--- asoc/codecs/wsa881x-analog.c
+++ asoc/codecs/wsa881x-analog.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2015-2016, 2018-2020, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/clk.h>
@@ -26,7 +26,6 @@
 #include <linux/i2c.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
-#include <soc/internal.h>
 #include <linux/regmap.h>
 #include <asoc/msm-cdc-pinctrl.h>
 #include "wsa881x-analog.h"
