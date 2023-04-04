4b8497c773444677e5504156900d20af3d54b44e
Ganapathiraju Sarath Varma <quic_ganavarm@quicinc.com>
2023-04-04 21:45:18 +0530

    SOC: Update swr master num port based on ports enabled.
    During swr disconnect port, swr master num port is
    updated based on the portinfo we receive from slave.
    Instead update the master num port based on the ports
    enabled. and also if requested port is not enabled
    continue to check for other enable port instead of
    returning error.
    
    Change-Id: Ia8a6e4935df443f9833e01d56195b590afe3bb4a
    Signed-off-by: Ganapathiraju Sarath Varma <quic_ganavarm@quicinc.com>

--- soc/swr-mstr-ctrl.c
+++ soc/swr-mstr-ctrl.c
@@ -1928,6 +1928,7 @@ static int swrm_disconnect_port(struct swr_master *master,
 	struct swrm_mports *mport;
 	struct swr_mstr_ctrl *swrm = swr_get_ctrl_data(master);
 	u8 mstr_port_id, mstr_ch_mask;
+	u8 num_port = 0;
 
 	if (!swrm) {
 		dev_err_ratelimited(&master->dev,
@@ -1960,7 +1961,7 @@ static int swrm_disconnect_port(struct swr_master *master,
 		if (!port_req) {
 			dev_err_ratelimited(&master->dev, "%s:port not enabled : port %d\n",
 					 __func__, portinfo->port_id[i]);
-			goto err;
+			continue;
 		}
 		port_req->req_ch &= ~portinfo->ch_en[i];
 		mport->req_ch &= ~mstr_ch_mask;
@@ -1970,12 +1971,13 @@ static int swrm_disconnect_port(struct swr_master *master,
 			mport->ch_rate = 0;
 			swrm_update_bus_clk(swrm);
 		}
+		num_port++;
 	}
 
-	if (master->num_port <= portinfo->num_port)
-		master->num_port = 0;
+	if (master->num_port > num_port)
+		master->num_port -= num_port;
 	else
-		master->num_port -= portinfo->num_port;
+		master->num_port = 0;
 	set_bit(DISABLE_PENDING, &swrm->port_req_pending);
 	swr_port_response(master, portinfo->tid);
 	mutex_unlock(&swrm->mlock);
