58a039c9e61a2eac35e0898b91063d6b59911d50
Prasad Kumpatla <quic_pkumpatl@quicinc.com>
2023-04-11 13:20:28 +0530

    asoc: wsa884x: add null check for wsa884x->component
    add null check before accessing for wsa884x->component.
    
    Change-Id: I0694ad6426317f2f80d5084125c5b24876f96a65
    Signed-off-by: Prasad Kumpatla <quic_pkumpatl@quicinc.com>

--- asoc/codecs/wsa884x/wsa884x.c
+++ asoc/codecs/wsa884x/wsa884x.c
@@ -2414,8 +2414,9 @@ static int wsa884x_swr_suspend(struct device *dev)
 	}
 	dev_dbg(dev, "%s: system suspend\n", __func__);
 	if (wsa884x->dapm_bias_off ||
+		(wsa884x->component &&
 		(snd_soc_component_get_bias_level(wsa884x->component) ==
-		 SND_SOC_BIAS_OFF)) {
+		 SND_SOC_BIAS_OFF))) {
 		msm_cdc_set_supplies_lpm_mode(dev, wsa884x->supplies,
 					wsa884x->regulator,
 					wsa884x->num_supplies,
