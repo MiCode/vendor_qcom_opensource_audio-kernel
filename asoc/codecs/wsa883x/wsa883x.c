7ae72e48970cdad1e92e8ad7d6d0e2f743ba3577
Vangala, Amarnath <quic_avangala@quicinc.com>
2023-08-03 12:43:27 +0530

    asoc: codecs: wsa883x: remove handling for pre ssr event
    Remove handling for PRE_SSR event and move gpio handling
     to SSR_UP event.
    
    Change-Id: Ia4dd87bdcf2f21376fa84e0f447d978ae222e816
    Signed-off-by: Vangala, Amarnath <quic_avangala@quicinc.com>

--- asoc/codecs/wsa883x/wsa883x.c
+++ asoc/codecs/wsa883x/wsa883x.c
@@ -1598,15 +1598,10 @@ static int wsa883x_event_notify(struct notifier_block *nb,
 		return -EINVAL;
 
 	switch (event) {
-	case BOLERO_SLV_EVT_PA_OFF_PRE_SSR:
-		if (test_bit(SPKR_STATUS, &wsa883x->status_mask))
-			snd_soc_component_update_bits(wsa883x->component,
-						WSA883X_PA_FSM_CTL,
-						0x01, 0x00);
-		wsa883x_swr_down(wsa883x);
-		break;
-
 	case BOLERO_SLV_EVT_SSR_UP:
+		wsa883x_swr_down(wsa883x);
+		usleep_range(500, 510);
+
 		wsa883x_swr_up(wsa883x);
 		/* Add delay to allow enumerate */
 		usleep_range(20000, 20010);
